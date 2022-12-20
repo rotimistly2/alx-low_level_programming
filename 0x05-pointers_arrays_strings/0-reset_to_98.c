82ce025bd2dff6b380d2c0f64a6e2d9374e47c42
rotimistly2 <rotimiaustin83@gmail.com>
2022-12-20 15:44:18 -0800

    my first main is main

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * reset_to_98 - funciton that takes a pointer to an integer parameter
- * @n: integer pointer
- * Return: 0 Success
+ * reset_to_98n - take a pointer to an int as parameter and updates
+ * the value point to 98
+ * @n: point to int
+ * Return: Nothing
  */
 
-void rest_to_98(int *n)
+void rest_to_98(int *n);
 {
 	*n = 98;
 }
